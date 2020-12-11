31ff0de394ad66fd089f26424a8896f7376e9a63
AlinMH <alin_florin.mihaila@stud.acs.upb.ro>
2020-12-11 17:35:58 +0200

    fixed part1

--- 2020/day11/main.py
+++ 2020/day11/main.py
@@ -3,25 +3,28 @@ from copy import deepcopy
 INPUT_FILE = "input"
 
 
-def are_adjancent(i, j, mat, seat_type):
+def num_adj_occ1(i, j, mat):
     max_i = len(mat) - 1
     max_j = len(mat[0]) - 1
 
-    up = mat[i - 1][j] == seat_type if i != 0 else True
-    down = mat[i + 1][j] == seat_type if i != max_i else True
-    left = mat[i][j - 1] == seat_type if j != 0 else True
-    right = mat[i][j + 1] == seat_type if j != max_j else True
-    up_left = mat[i - 1][j - 1] == seat_type if i != 0 and j != 0 else True
-    up_right = mat[i - 1][j + 1] == seat_type if i != 0 and j != max_j else True
-    down_left = mat[i + 1][j - 1] == seat_type if i != max_i and j != 0 else True
-    down_right = mat[i + 1][j + 1] == seat_type if i != max_i and j != max_j else True
+    directions = ((-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1))
+    num = 0
 
-    flags = [up, down, left, right, up_left, up_right, down_left, down_right]
+    for dx, dy in directions:
+        xp, yp = i + dx, j + dy
+        if (xp >= 0 and xp <= max_i) and (yp >= 0 and yp <= max_j):
+            view = mat[xp][yp]
+            if view != ".":
+                if view == "#":
+                    num += 1
+            xp += dx
+            yp += dy
 
-    return all(flags)
+    return num
 
 
-def num_adj_occ(i, j, mat):
+
+def num_adj_occ2(i, j, mat):
     max_i = len(mat) - 1
     max_j = len(mat[0]) - 1
 
@@ -56,8 +59,10 @@ def part1():
                 for j, column in enumerate(row):
                     if column == ".":
                         continue
-                    elif are_adjancent(i, j, prev, column):
-                        current[i][j] = "#" if column == "L" else "L"
+                    elif column == "L" and num_adj_occ1(i, j, prev) == 0:
+                        current[i][j] = "#"
+                    elif column == "#" and num_adj_occ1(i, j, prev) >= 4:
+                        current[i][j] = "L"
 
             if prev == current:
                 occupied = 0
@@ -85,9 +90,9 @@ def part2():
                 for j, column in enumerate(row):
                     if column == ".":
                         continue
-                    elif column == "L" and num_adj_occ(i, j, prev) == 0:
+                    elif column == "L" and num_adj_occ2(i, j, prev) == 0:
                         current[i][j] = "#"
-                    elif column == "#" and num_adj_occ(i, j, prev) >= 5:
+                    elif column == "#" and num_adj_occ2(i, j, prev) >= 5:
                         current[i][j] = "L"
 
             if prev == current:
@@ -103,8 +108,8 @@ def part2():
 
 
 if __name__ == "__main__":
-    # result = part1()
-    # print(result)
+    result = part1()
+    print(result)
 
     result = part2()
     print(result)
